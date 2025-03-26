10d9f200c7630346416a35742f6d93284f53ca3a
Alireza <alirezakhdb1383@gmail.com>
2025-03-26 15:28:43 -0700

    update image

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -6,7 +6,7 @@ export const Title = ()=>{
       </div>
       <div>
         <div className="max-md:block mx-auto hidden w-[300px]">
-          <img src="image/programmer-bro.svg" className="" alt="" />
+          <img src="image/Programmer-bro.svg" className="" alt="" />
         </div>
         <div className="py-52 max-xl:py-32 max-lg:py-16 max-md:py-4 w-1/2 max-md:w-fit max-md:px-4">
           <h1 className="text-6xl mb-3 h_title max-lg:text-4xl max-md:text-3xl max-sm:text-2xl">
